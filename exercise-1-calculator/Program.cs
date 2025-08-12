f722aa8384c049b5df88218ada8d3ab8e4aa54f3
Lars Henrik <larshenrikknapper@gmail.com>
2025-08-12 14:23:45 +0200

    Added switch cases for opperators

--- exercise-1-calculator/Program.cs
+++ exercise-1-calculator/Program.cs
@@ -7,6 +7,55 @@ class Calculator
 {
     static void Main()
     {
-        Console.WriteLine("This is a calculator!!!!");
+        Console.WriteLine("===== Simple Calculator =====");
+
+
+
+        Console.Write("Enter first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter operation (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+        Console.Write("Enter second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+
+        double result = 0;
+        bool validOperation = true;
+
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+
+            case "-":
+                result = num1 - num2;
+                break;
+
+            case "*":
+                result = num1 * num2;
+                break;
+
+            case "/":
+                result = num1 / num2;
+                break;
+                
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
