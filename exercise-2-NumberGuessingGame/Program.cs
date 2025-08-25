e8c9338f12349809638d5e487619d41818b5115a
Lars Henrik <larshenrikknapper@gmail.com>
2025-08-25 13:44:18 +0200

    Jobbet på exercise 2

--- exercise-2-NumberGuessingGame/Program.cs
+++ exercise-2-NumberGuessingGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+
 using System;
 
 class GuessingGame
@@ -10,44 +11,11 @@ class GuessingGame
         Console.WriteLine("I'm thinking of a number between 1 and 100!");
 
         Random random = new Random();
-        int secretNumber = random.Next(1, 101);
-        int attempts = 0;
-        int maxAttempts = 7;
+        int secretNumber = random.Next();
+        int attemts = 0;
+        int Max attemts = 7;
         bool hasWon = false;
 
-        while (attempts < maxAttempts && !hasWon)
-        {
-            Console.Write($"Attempt {attempts + 1}/{maxAttempts} - Enter your guess: ");
 
-            if (int.TryParse(Console.ReadLine(), out int guess))
-            {
-                attempts++;
-
-                if (guess == secretNumber)
-                {
-                    Console.WriteLine($"🎉 Congratulations! You guessed it in {attempts} attempts!");
-                    hasWon = true;
-                }
-                else if (guess < secretNumber)
-                {
-                    Console.WriteLine("Too low! Try a higher number.");
-                }
-                else
-                {
-                    Console.WriteLine("Too high! Try a lower number.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid number!");
-            }
-        }
-
-        if (!hasWon)
-        {
-            Console.WriteLine($"😔 Game over! The number was {secretNumber}");
-        }
-
-        Console.WriteLine("Thanks for playing!");
     }
 }
