4b15ba2975f9f2a783859eaf03e6792ff5b8f59a
Lars Henrik <larshenrikknapper@gmail.com>
2026-03-04 18:20:20 +0100

    Worked on math operators.

--- The_C#_Players_Guide/math/Program.cs
+++ The_C#_Players_Guide/math/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+float area;
+float baseSize;
+float hight;
+
+/*
+Console.WriteLine("What is the base size of the triangle?: ");
+baseSize = float.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the hight of the triangle?: ");
+hight = float.Parse(Console.ReadLine());
+
+area = (baseSize * hight) / 2;
+
+Console.WriteLine("The area of the triangle is: " + area);
+
+
+int a = 5;
+int b = 2;
+int result = a / b;
+Console.WriteLine(result);
+*/
+
+int totalEggs;
+int numberOfSisters = 4;
+float remainder;
+int eggsEach;
+
+Console.WriteLine("How many eggs today?: ");
+totalEggs = int.Parse(Console.ReadLine());
+
+eggsEach = totalEggs / numberOfSisters;
+
+remainder = totalEggs % numberOfSisters;
+
+Console.WriteLine("The sisters should get: " + eggsEach + " eggs each.");
+Console.WriteLine("The duckbear should get: " + remainder + " eggs.");
+
+
+
+
