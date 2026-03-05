5f55f91e0380103dbfa1300621c625b868502fbf
Lars Henrik <larshenrikknapper@gmail.com>
2026-03-05 14:44:13 +0100

    Calculate deploy location based on chess board.

---
+++ The_C#_Players_Guide/StringsSkills/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\"");
+Console.WriteLine(@"\");
+Console.WriteLine("Line 1 \nLine 2");
+
+string name1 = Console.ReadLine();
+string name2 = Console.ReadLine();
+Console.WriteLine($"#1: {name1,20}");
+Console.WriteLine($"#2: {name2,20}");
+
+Console.WriteLine($"{Math.PI:0.000}");
+
+float currentHealth = 4;
+float maxHealth = 9;
+Console.WriteLine($"{currentHealth / maxHealth:0.0%}");
---
+++ The_C#_Players_Guide/TheDefenceOfConsolas/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
+Console.Title = "Defence of Consolas";
+
+
+int targetRow;
+int targetColumn;
+
+int deployLocation1Row;
+int deployLocation1Column;
+
+int deployLocation2Row;
+int deployLocation2Column;
+
+int deployLocation3Row;
+int deployLocation3Column;
+
+int deployLocation4Row;
+int deployLocation4Column;
+
+Console.WriteLine("target row?: ");
+targetRow = int.Parse(Console.ReadLine());
+Console.WriteLine("targetColumn?: ");
+targetColumn = int.Parse(Console.ReadLine());
+
+deployLocation1Row = targetRow;
+deployLocation1Column = targetColumn - 1;
+
+deployLocation2Row = targetRow - 1;
+deployLocation2Column = targetColumn;
+
+deployLocation3Row = targetRow;
+deployLocation3Column = targetColumn + 1;
+
+deployLocation4Row = targetRow + 1;
+deployLocation4Column = targetColumn;
+
+Console.Beep();
+
+Console.WriteLine("Deploy to: \n");
+Console.WriteLine($"{(deployLocation1Row, deployLocation1Column)}");
+Console.WriteLine($"{(deployLocation2Row, deployLocation2Column)}");
+Console.WriteLine($"{(deployLocation3Row, deployLocation3Column)}");
+Console.WriteLine($"{(deployLocation4Row, deployLocation4Column)}");
