9a1c144c8b7954bdfd3e3130a9914a52635dd638
Lars Henrik <larshenrikknapper@gmail.com>
2026-03-05 13:41:31 +0100

    Ferdig med oppgaven The Dominion of Kings

--- The_C#_Players_Guide/TheDominionOfKings_Challange/Program.cs
+++ The_C#_Players_Guide/TheDominionOfKings_Challange/Program.cs
@@ -1,2 +1,25 @@
 ﻿
 
+int pointsPerEstate = 1;
+int pointsPerDuchy = 3;
+int pointsPerProvince = 6;
+
+
+int userEstates;
+int userDuchy;
+int userProvince;
+
+int userTotal;
+
+Console.WriteLine("How many estates do you have?: ");
+userEstates = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many duchy's do you have?: ");
+userDuchy = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many provinces do you have?: ");
+userProvince = int.Parse(Console.ReadLine());
+
+userTotal = (userEstates * pointsPerEstate) + (userDuchy * pointsPerDuchy) + (userProvince * pointsPerProvince);
+
+Console.WriteLine(userTotal);
