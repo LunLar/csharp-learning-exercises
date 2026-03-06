da591e9359887dac69d54c0ce150cfc8642dcb58
Lars Henrik <larshenrikknapper@gmail.com>
2026-03-06 15:46:15 +0100

    WatchTower Challange

---
+++ The_C#_Players_Guide/Watchtower_Challange/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int x;
+int y;
+
+
+Console.WriteLine("Give me a X value");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Give me a Y value");
+y = Convert.ToInt32(Console.ReadLine());
+
+string messageFromTower;
+
+if (y > 0 && x < 0)
+    messageFromTower = "North West";
+
+else if (y > 0 && x == 0)
+    messageFromTower = "North";
+
+else if (y > 0 && x > 0)
+    messageFromTower = "North East";
+
+else if (y == 0 && x < 0)
+    messageFromTower = "West";
+
+else if (y == 0 && x == 0)
+    messageFromTower = "They are here!";
+
+else if (y == 0 && x > 0)
+    messageFromTower = "East";
+
+else if (y < 0 && x < 0)
+    messageFromTower = "South West";
+else if (y < 0 && x == 0)
+    messageFromTower = "South";
+else
+    messageFromTower = "South East";
+
+Console.WriteLine(messageFromTower);
+
+
+
