1bf3488f36b507fcb32e3bb8544817da48b8b2bf
Lars Henrik <larshenrikknapper@gmail.com>
2026-03-06 12:34:31 +0100

    Worked on Repairing the Clocktower Challange.

---
+++ The_C#_Players_Guide/IfStatements/Program.cs
@@ -0,0 +1,21 @@
+﻿ 
+
+using System.ComponentModel.Design;
+
+string tick = "Tick";
+string tock = "Tock";
+
+int userInput;
+
+Console.WriteLine("Give me a number: ");
+userInput = Convert.ToInt32(Console.ReadLine());
+
+if (userInput % 2 == 0)
+{
+    Console.WriteLine(tick);
+}
+
+else
+{
+    Console.WriteLine(tock);
+}
